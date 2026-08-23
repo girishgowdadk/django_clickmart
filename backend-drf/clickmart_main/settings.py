5b4cc543e4d6925fa56843e9b7273aabcb6391ae
girishgowdadk <girishg628@gmail.com.com>
2026-08-23 13:36:09 +0000

    changes

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
 
 # Application definition
 
@@ -171,6 +172,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://64.227.139.134:5173'
 ]
